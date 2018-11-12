244ac64da7ba9b4a0f505ff987518fbb74b4814b
Abdul  Ismail <abdulismail@Abduls-MacBook-Air.local>
2018-11-12 16:19:01 -0600

    displaying images on the dom

--- client.js
+++ client.js
@@ -1,1 +1,26 @@
 console.log(people);
+
+$( document ).ready(onReady);
+
+function onReady(){
+    console.log('Jquery');
+    appendPeople();
+}
+
+function appendPeople(){
+    console.log('In appendPeople');
+
+    for( let person of people ){
+        let div = $('<div>');
+        div.data(`personInfo`, person)
+        div.append(imgTag(person));
+        $('#container').append(div);
+    }
+
+}
+
+function imgTag(obj){
+    console.log('In personUrl', obj.name);
+    return `<img src="https://github.com/${obj.githubUsername}.png?size=250"
+        alt="Profile image of ${obj.name}">`;
+}
